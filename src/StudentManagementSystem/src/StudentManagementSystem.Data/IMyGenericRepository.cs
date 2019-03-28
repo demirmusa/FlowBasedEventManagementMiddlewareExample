cc8bfdc00e781aaaffdce96e2c548fc1bc4c16b3
musademir <demirmusa96@gmail.com>
2019-03-28 23:23:47 +0300

    MyGenericRepository which inherited from GenericRepository created
    not to call SMSDbContext in everywhere i use GenericRepository  i created MyGenericRepository which inherited from GenericRepository. it will handle calling context from now on.

---
+++ src/StudentManagementSystem/src/StudentManagementSystem.Data/IMyGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using Example.EFCoreShared;
+using Example.EFCoreShared.interfaces;
+using StudentManagementSystem.Data;
+
+namespace StudentManagementSystem.Business
+{
+    public interface IMyGenericRepository<TEntity> : IGenericRepository<SMSDbContext, TEntity>
+        where TEntity : BaseDbEntity
+    {
+
+    }
+}
---
+++ src/StudentManagementSystem/src/StudentManagementSystem.Data/MyGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Example.EFCoreShared;
+using StudentManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagementSystem.Business
+{
+    public class MyGenericRepository<TEntity> : GenericRepository<SMSDbContext, TEntity>, IMyGenericRepository<TEntity>
+        where TEntity : BaseDbEntity
+    {
+        public MyGenericRepository(SMSDbContext context) : base(context)
+        {
+            //context.Database.EnsureCreated();
+        }
+    }
+}
