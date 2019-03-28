caa2567e1abdb9ae9b71c876ea6637b96ccc0bac
musademir <demirmusa96@gmail.com>
2019-03-28 20:16:29 +0300

    generic repository unit test

---
+++ src/StudentManagementSystem/test/StudentManagementNUnitTest/GenericRepositoryTests.cs
@@ -0,0 +1,156 @@
+using Example.EFCoreShared;
+using Example.EFCoreShared.interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using StudentManagementSystem.Data;
+using StudentManagementSystem.Data.dbEntities;
+using System;
+using System.Linq;
+
+namespace Tests
+{
+    public class Tests
+    {
+        IGenericRepository<SMSDbContext, Person> _personRepo;
+        IGenericRepository<SMSDbContext, StudentCourseScore> _studentCourseScoreRepo;
+        [SetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+
+            services.AddDbContext<SMSDbContext>(options =>
+                options.UseSqlServer("Data Source=LAPTOP-3O58F4FN;database=studentManagement;trusted_connection=yes;"));
+
+
+            services.AddGenericRepositorySingleton();
+
+            var provider = services.BuildServiceProvider();
+
+            _personRepo = provider.GetService<IGenericRepository<SMSDbContext, Person>>();
+            _studentCourseScoreRepo = provider.GetService<IGenericRepository<SMSDbContext, StudentCourseScore>>();
+        }
+
+        [Test]
+        public void PersonCreate()
+        {
+            try
+            {
+                _personRepo.Insert(new Person
+                {
+                    Name = "Musa",
+                    Surname = "DEMÝR"
+                });
+            }
+            catch (Exception e)
+            {
+                Assert.That(false, "Error: " + e.Message);
+            }
+            Assert.Pass();
+
+        }
+
+        [Test]
+        public void PersonUpdate()
+        {
+            try
+            {
+                var person = _personRepo.AsQueryable().FirstOrDefault();
+                person.Name = person.Name + " " + DateTime.Now;
+                _personRepo.Update(person);
+            }
+            catch (Exception e)
+            {
+                Assert.That(false, "Error: " + e.Message);
+            }
+            Assert.Pass();
+        }
+        [Test]
+        public void PersonDelete()
+        {
+            try
+            {
+                var person = _personRepo.AsQueryable().OrderByDescending(x => x.ID).FirstOrDefault();
+                _personRepo.Delete(person.ID);
+            }
+            catch (Exception e)
+            {
+                Assert.That(false, "Error: " + e.Message);
+            }
+            Assert.Pass();
+        }
+        [Test]
+        public void PersonAddOrUpdate()
+        {
+            try
+            {
+                var person = _personRepo.AddOrUpdate(new Person
+                {
+                    Name = "Musa",
+                    Surname = "DEMÝR " + DateTime.Now
+                });
+
+                person.Name = person.Name + " - " + DateTime.Now;
+                _personRepo.AddOrUpdate(person);
+            }
+            catch (Exception e)
+            {
+                Assert.That(false, "Error: " + e.Message);
+            }
+            Assert.Pass();
+        }
+        [Test]
+        public void SoftUpdateableCreate()
+        {
+            try
+            {
+                var score = _studentCourseScoreRepo.Insert(new StudentCourseScore
+                {
+                    FKCourseID = 1,
+                    FKStudentID = 2
+                });
+            }
+            catch (Exception e)
+            {
+                Assert.That(false, "Error: " + e.Message);
+            }
+            Assert.Pass();
+        }
+        [Test]
+        public void SoftUpdateableUpdate()
+        {
+            try
+            {
+                var score = _studentCourseScoreRepo.AsQueryable().FirstOrDefault();
+
+                score.FKStudentID = 456;
+                _studentCourseScoreRepo.Update(score);
+            }
+            catch (Exception e)
+            {
+                Assert.That(false, "Error: " + e.Message);
+            }
+            Assert.Pass();
+        }
+        [Test]
+        public void SoftUpdateableAddOrUpdate()
+        {
+            try
+            {
+                var score = _studentCourseScoreRepo.AddOrUpdate(new StudentCourseScore
+                {
+                    FKCourseID = 5,
+                    FKStudentID = 8
+                });
+
+                score.FKStudentID = 99;
+                _studentCourseScoreRepo.AddOrUpdate(score);
+            }
+            catch (Exception e)
+            {
+                Assert.That(false, "Error: " + e.Message);
+            }
+            Assert.Pass();
+        }
+    }
+}
